19a573599493c0f12992afb22cb61ea0d4c38ac5
www-data <www-data@fhcrc.org>
2014-06-18 12:49:06 -0700

    modified celery startup scripts for our config. see also the crontab
    for www-data

--- conf/server/celery.beat.sh
+++ conf/server/celery.beat.sh
@@ -2,11 +2,11 @@
 set -ue
 
 # This is required so that the default configuration file works.
-source /home/www/sites/biostar-central/live/deploy.env
+source /home/www-data/biostar-central/live/custom.env
 
 # Setting the various access logs.
-ACCESS_LOG=/home/www/sites/biostar-central/live/logs/celery-access.log
-ERROR_LOG=/home/www/sites/biostar-central/live/logs/celery-error.log
+ACCESS_LOG=/home/www-data/biostar-central/live/logs/celery-access.log
+ERROR_LOG=/home/www-data/biostar-central/live/logs/celery-error.log
 
 # The user and group the unicorn process will run as.
 NUM_WORKERS=3
@@ -15,7 +15,7 @@ NUM_WORKERS=3
 NAME="biostar"
 
 # The gunicorn instance to run.
-CELERY="/home/www/.virtualenvs/biostar/bin/celery"
+CELERY="/home/www-data/.virtualenvs/biostarsenv/bin/celery"
 
 echo "starting celery beat with DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE"
 
--- conf/server/celery.worker.sh
+++ conf/server/celery.worker.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -ue
 
+
 # This is required so that the default configuration file works.
-source /home/www/sites/biostar-central/live/deploy.env
+source /home/www-data/biostar-central/live/custom.env
 
 # Setting the various access logs.
-ACCESS_LOG=/home/www/sites/biostar-central/live/logs/celery-access.log
-ERROR_LOG=/home/www/sites/biostar-central/live/logs/celery-error.log
+ACCESS_LOG=/home/www-data/biostar-central/live/logs/celery-access.log
+ERROR_LOG=/home/www-data/biostar-central/live/logs/celery-error.log
 
 # The user and group the unicorn process will run as.
 NUM_WORKERS=3
@@ -15,7 +16,7 @@ NUM_WORKERS=3
 NAME="biostar"
 
 # The gunicorn instance to run.
-CELERY="/home/www/.virtualenvs/biostar/bin/celery"
+CELERY="/home/www-data/.virtualenvs/biostarsenv/bin/celery"
 
 echo "starting celery worker with DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE"
 
