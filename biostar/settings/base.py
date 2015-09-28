78f5c0afcc84457eb3babf06b3af620edfe33ec4
Dan Tenenbaum <dtenenba@fhcrc.org>
2015-09-28 10:34:12 -0700

    don't require BIOSTAR_IP be set

--- biostar/settings/base.py
+++ biostar/settings/base.py
@@ -124,8 +124,11 @@ TEMPLATE_STRING_IF_INVALID = "*** MISSING ***"
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", get_env("BIOSTAR_HOSTNAME"),
-  get_env('BIOSTAR_IP')]
+ALLOWED_HOSTS = ["localhost", get_env("BIOSTAR_HOSTNAME")]
+
+if BIOSTAR_IP in os.environ:
+    ALLOWED_HOSTS.append(os.environ['BIOSTAR_IP'])
+
 
 ATOMIC_REQUESTS = True
 CONN_MAX_AGE = 10;
